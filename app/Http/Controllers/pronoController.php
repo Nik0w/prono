9bb3eecf1f73bd293c94a8511e4a2ebb7cd235d1
Nikow <nicolas.larrode@gmail.com>
2018-07-24 17:28:43 +0200

    ajout de la page Pronos

---
+++ app/Http/Controllers/pronoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class pronoController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('pronos');
+    }
+}
